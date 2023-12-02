b65dfbaf8588233cff6f6cf3379f40cbaa37e87b
Pony <pony076152340@gmail.com>
2023-12-02 11:36:23 +0800

    draw validation

--- app/api/pool/util/draw.py
+++ app/api/pool/util/draw.py
@@ -2,7 +2,10 @@ from random import randint
 from app.database.util import update, get, create
 from app.utils.auth.auth_util import get_login_user
 from app.utils.time_util import get_current, date2str
-
+from app.database.model.account import (
+    get_account_by_student_id,
+    update_account_by_id
+)
 from .pool import PoolItem
 
 from flask import request
@@ -13,8 +16,8 @@ from .get import get_pool_item
 
 def draw_card(pool_id:str):
     payload: dict = request.json
-
     type = payload["type"]
+
 
     item_list = get_pool_item(pool_id)
 
@@ -25,12 +28,17 @@ def draw_card(pool_id:str):
     elif type == DrawType.multi.value:
         num = 10
 
+
+    user = get_login_user()
+    update_account_by_id(user["id"], {"bocoin":user.get("bocoin") - (10 * num) })
+
     res = []
     print(item_list)
     for i in range(num):
         picked = 0
-        if len(item_list) >= 1:
+        if len(item_list) > 1:
             picked = randint(0, len(item_list) - 1)
+
         res.append(item_list[picked])
     print(num, res)
 
